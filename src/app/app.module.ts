70338e3a66c72b51a51e393a8a6b4d81f2c39c46
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-06-03 13:08:43 -0400

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketdetailsComponent } from './ticketdetails/ticketdetails.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketdetailsComponent,
+    TicketsComponent,
+    BookmarksComponent
   ],
   imports: [
     BrowserModule,
