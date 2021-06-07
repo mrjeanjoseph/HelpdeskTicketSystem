e99e060a5113d3b0731e8d28a0b13fa5097e1d9a
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-06-07 09:13:48 -0400

    updates-3

--- src/app/ticket.ts
+++
@@ -1,8 +0,0 @@
-export interface Ticket {
-    [x: string]: any; // Look into why this is here
-    id: number;
-    ticketName: string;
-    status: boolean;
-    issue: string;
-    openedBy: string;    
-}
